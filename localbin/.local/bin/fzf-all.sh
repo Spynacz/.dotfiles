135abbc1f2c6254961d7156b95eaebf5a59760f4
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-11-02 16:47:35 +0100

    Add direct file editing from fzf-all, other changes

--- localbin/.local/bin/fzf-all.sh
+++ localbin/.local/bin/fzf-all.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-<<<<<<< HEAD
-selected=$(fd . ~ --type d -H -E .cache | fzf +m)
-=======
 
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
@@ -9,7 +6,6 @@ export FZF_DEFAULT_OPTS=" \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 selected=$(fd . ~ --type f --type d --type l --hidden --exclude "*[Cc]ache*" | fzf +m)
->>>>>>> 91fb83f (Add direct file editing from fzf-all, other changes)
 
 if [ -z "$selected" ]; then
     exit 0
