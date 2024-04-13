9fb31bd6ae4b8f42f609e27ebab7362485b7b4ef
Spynacz <63506536+Spynacz@users.noreply.github.com>
2024-04-13 11:50:53 +0200

    Remove appimages, sort scripts

--- localbin/.local/bin/fzf-some.sh
+++ localbin/.local/bin/fzf-some.sh
@@ -5,8 +5,9 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-selected=$(fd . ~ ~/Dev ~/Dev/Java ~/Documents ~/Documents/RPG ~/Documents/RPG/DND \
-    ~/Documents/Studia/Semestr* --exact-depth 1 --type d | fzf +m)
+position="down"
+
+selected=$(fd . ~ ~/Dev ~/Dev/Java ~/Documents ~/Documents/RPG ~/Documents/RPG/DND ~/Documents/Studia/Semestr* --exact-depth 1 --type d | fzf +m)
 
 if [ -z "$selected" ]; then
     exit 0
@@ -15,4 +16,4 @@ fi
 selected_name=$(basename "$selected")
 
 kitty @ focus-tab --match "title:^$selected_name$" ||
-    kitty @ launch --type=tab --hold --cwd="$selected" --title="$selected_name" --tab-title="$selected_name"
+kitty @ launch --type=tab --hold --cwd="$selected" --title="$selected_name" --tab-title="$selected_name"
